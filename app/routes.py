3cc61c8eefe073b9e25f60fc8060e4d39e27988f
Lux Barker <lux.barker13@gmail.com>
2021-10-22 15:13:55 -0700

    handle POST request for new book

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,17 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
