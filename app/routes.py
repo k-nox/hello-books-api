1c1aa0aa8af10f711f721a7a1a135714067f636b
Lux Barker <lux.barker13@gmail.com>
2021-10-22 15:28:32 -0700

    handle GET request for all books

--- app/routes.py
+++ app/routes.py
@@ -5,13 +5,19 @@ from flask import Blueprint, jsonify, make_response, request
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = [{"id": book.id, "title": book.title,
+                           "description": book.description} for book in books]
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Book {new_book.title} successfully created", 201)
